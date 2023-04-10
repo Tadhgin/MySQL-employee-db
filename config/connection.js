01f657604c20088496045f4f15700634a49ef3ab
Tadhgin_White <teddy_bear_morbid@hotmail.com>
2023-04-10 13:26:19 +1000

    debug

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,13 @@
 const mysql = require("mysql");
 var connection = mysql.createConnection({
-	// Connection
-	host: "localhost",
-	port: 3306,
-	// MySQL Workbench
-	user: "root",
-	password: "12345678",
-	// Database created in schemea.sql
-	database: "employeesDB",
+    // Connection
+    host: "localhost",
+    port: 3306,
+    // MySQL Workbench
+    user: "root",
+    password: PROCESS.ENV.DB_PASSWORD,
+    // Database created in schemea.sql
+    database: "employeesDB",
 });
 
 // connect to the mysql server and database
